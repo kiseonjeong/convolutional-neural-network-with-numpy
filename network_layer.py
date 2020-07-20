df3237119ac9238280369f372e4c9bd0b6943dd2
Kiseon Jeong <glenjeong6418@gmail.com>
2020-07-21 01:09:01 +0900

    update network_layer.py for testing convolution layer

--- network_layer.py
+++ network_layer.py
@@ -1,9 +1,35 @@
 import numpy as np
+from numpy.lib import stride_tricks
 from activation_function import ActFunc
 from cost_function import CostFunc
 
+# The addition layer
+class AddLayer:
+    """ 
+    Addition layer
+    """
+    # Object initializer
+    def __init__(self):
+        pass
+
+    # Do forward computations
+    def forward(self, x, y):
+        out = x + y
+
+        return out
+
+    # Do backward computations
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
+
 # The multiplication layer
 class MulLayer:
+    """ 
+    Multiplication layer
+    """
     # Object initializer
     def __init__(self):
         self.x = None
@@ -24,27 +50,33 @@ class MulLayer:
 
         return dx, dy
 
-# The addition layer
-class AddLayer:
+# The sigmoid layer
+class Sigmoid:
+    """ 
+    Sigmoid (Logistic function) layer
+    """
     # Object initializer
     def __init__(self):
-        pass
+        self.out = None
 
     # Do forward computations
-    def forward(self, x, y):
-        out = x + y
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
 
         return out
 
     # Do backward computations
     def backward(self, dout):
-        dx = dout * 1
-        dy = dout * 1
+        dx = dout * (1.0 - self.out) * self.out
 
-        return dx, dy
+        return dx
 
 # The ReLU layer
 class Relu:
+    """ 
+    ReLU layer
+    """
     # Object initializer
     def __init__(self):
         self.mask = None
@@ -64,37 +96,24 @@ class Relu:
 
         return dx
 
-# The sigmoid layer
-class Sigmoid:
-    # Object initializer
-    def __init__(self):
-        self.out = None
-
-    # Do forward computations
-    def forward(self, x):
-        out = 1 / (1 + np.exp(-x))
-        self.out = out
-
-        return out
-
-    # Do backward computations
-    def backward(self, dout):
-        dx = dout * (1.0 - self.out) * self.out
-
-        return dx
-
 # The affine layer
 class Affine:
+    """ 
+    Affine layer
+    """
     # Object initializer
     def __init__(self, W, b):
         self.W = W
         self.b = b
         self.x = None
+        self.original_x_shape = None
         self.dW = None
         self.db = None
 
     # Do forward computations
     def forward(self, x):
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
         self.x = x
         out = np.dot(self.x, self.W) + self.b
 
@@ -105,11 +124,15 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+        dx = dx.reshape(*self.original_x_shape)
 
         return dx
 
 # The softmax with loss layer
 class SoftmaxWithLoss:
+    """ 
+    Softmax with cross entorpy error layer
+    """
     # Object initializer
     def __init__(self):
         self.loss = None
@@ -135,6 +158,13 @@ class SoftmaxWithLoss:
 
 # The dropout layer
 class Dropout:
+    """ 
+    Dropout regularization layer
+
+    Parameters
+    ----------
+    dropout_ratio : 0.0 (no dropout), 1.0 (all dropout)\n
+    """
     # Object initializer
     def __init__(self, dropout_ratio=0.5):
         self.dropout_ratio = dropout_ratio
@@ -154,6 +184,17 @@ class Dropout:
 
 # The batch-normalization layer
 class BatchNorm:
+    """ 
+    Batch normalization layer
+
+    Parameters
+    ----------
+    gamma : scale parameter\n
+    beta : shift parameter\n
+    momentum : moving average parameter\n
+    running_mean : moving average result of batch means\n
+    running_var : moving average result of batch variances\n
+    """
     # Object initializer
     def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
         self.gamma = gamma
@@ -232,3 +273,95 @@ class BatchNorm:
         self.dbeta = dbeta
 
         return dx
+
+# The convolution layer
+class Convolution:
+    """ 
+    Convolution layer
+
+    Parameters
+    ----------
+    stride : sliding interval\n
+    pad : data padding length\n
+    """
+    # Object initializer
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    # Do forward computations
+    def forward(self, x):
+        # Calculate output resolution information
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2 * self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2 * self.pad - FW) / self.stride)
+
+        # Apply the im2col
+        col = self.__im2col(x, FH, FW)
+        col_W = self.W.reshape(FN, -1).T
+
+        # Calculate forward computations like affine layer
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    # Convert image to column
+    def __im2col(self, input_data, filter_h, filter_w):
+        # Calculate result resolution information
+        N, C, H, W = input_data.shape
+        out_h = (H + 2 * self.pad - filter_h) // self.stride + 1
+        out_w = (W + 2 * self.pad - filter_w) // self.stride + 1
+
+        # Do padding on the input data
+        img = np.pad(input_data, [(0, 0), (0, 0), (self.pad, self.pad), (self.pad, self.pad)], 'constant')
+        col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+        # Generate the column data
+        for y in range(filter_h):
+            y_max = y + self.stride * out_h
+            for x in range(filter_w):
+                x_max = x + self.stride * out_w
+                col[:, :, y, x, :, :] = img[:, :, y:y_max:self.stride, x:x_max:self.stride]
+        col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+
+        return col
+
+    # Do backward computations
+    def backward(self, dout):
+        # Calculate output resolution information
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+
+        # Calculate gradients
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+        dcol = np.dot(dout, self.col_W.T)
+        dx = self.__col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+    # Convert column to image
+    def __col2im(self, col, input_shape, filter_h, filter_w):
+        # Calculate result resolution information
+        N, C, H, W = input_shape.shape
+        out_h = (H + 2 * self.pad - filter_h) // self.stride + 1
+        out_w = (W + 2 * self.pad - filter_w) // self.stride + 1
+        col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+        # Generate the image data
+        img = np.zeros((N, C, H + 2 * self.pad + self.stride - 1, W + 2 * self.pad + self.stride - 1))
+        for y in range(filter_h):
+            y_max = y + self.stride * out_h
+            for x in range(filter_w):
+                x_max = x + self.stride * out_w
+                img[:, :, y:y_max:self.stride, x:x_max:self.stride] += col[:, :, y, x, :, :]
+
+        return img[:, :, self.pad:H + self.pad, self.pad:W + self.pad]
