902307699d59a0a38d45519a0bacd014ba705727
Kiseon Jeong <glenjeong6418@gmail.com>
2020-10-09 12:51:29 +0900

    add visualizations

--- train_simple_conv_net.py
+++ train_simple_conv_net.py
@@ -20,7 +20,7 @@ dataset_test = Dataset(x_test, t_test)
 max_epochs = 20
 network = SimpleConvNet(input_dim=(1, 28, 28), conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                         num_hidden_node=100, num_output_node=10, weight_init_std=0.01)
-trainer = NetTrainer(dataset_train, dataset_test, network, max_epochs, mini_batch_size=100, optimizer='Adam', optimizer_param={'lr': 0.001})
+trainer = NetTrainer(dataset_train, dataset_test, network, max_epochs, mini_batch_size=100, optimizer='Adam', optimizer_param={'lr': 0.001}, eval_sample_num_per_epoch=1000)
 
 # Do training on the network
 trainer.train_network()
